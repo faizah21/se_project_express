05cf449592f14026abeef1b3693c1722c3856b22
Faizah Ansar <faizahansar23@gmail.com>
2024-12-04 17:59:17 -0500

    new Updates

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -25,13 +25,13 @@ const clothingItem = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "user",
     required: true,
   },
   likes: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "user",
     },
   ],
   createdAt: {
